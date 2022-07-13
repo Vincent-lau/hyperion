0fc75e57b15d6db2b759b97f0e9167396d8f7ed7
Shuntian Liu <shuntian.liu@outlook.com>
2022-07-13 17:11:55 +0100

    style:move script files

---
+++ script/getlogs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from kubernetes import client, config
+import os
+
+# Configs can be set in Configuration class directly or using helper utility
+config.load_kube_config()
+
+v1 = client.CoreV1Api()
+print("Listing pods with their IPs:")
+ret = v1.list_pod_for_all_namespaces(watch=False)
+for i in ret.items:
+    if i.metadata.name.startswith("my-scheduler-"):
+      print(f"{i.metadata.name} {i.status.pod_ip}")
+      print(v1.read_namespaced_pod_log(name = i.metadata.name, namespace = i.metadata.namespace))
+      print()
