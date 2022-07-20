66f6563d0ed906716959c86f6eea7252fc06a179
Shuntian Liu <shuntian.liu@outlook.com>
2022-07-20 17:47:20 +0100

    chore:get controller log

--- script/getlogs.py
+++ script/getlogs.py
@@ -10,7 +10,7 @@ v1 = client.CoreV1Api()
 print("Listing pods with their IPs:")
 ret = v1.list_pod_for_all_namespaces(watch=False)
 for i in ret.items:
-    if i.metadata.name.startswith("my-scheduler-"):
+    if i.metadata.name.startswith("my-scheduler-") or i.metadata.name.startswith("my-controller"):
       print(f"{i.metadata.name} {i.status.pod_ip}")
       print(v1.read_namespaced_pod_log(name = i.metadata.name, namespace = i.metadata.namespace))
       print()
