5987d04fc3386e7ec605624c84e4034bb22ade2f
Shuntian Liu <shuntian.liu@outlook.com>
2022-07-22 17:47:58 +0100

    chore: detailed logging

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"example/dist_sched/config"
 	"example/dist_sched/controller"
 	"flag"
 	"math/rand"
 	"time"
+
 	log "github.com/sirupsen/logrus"
 )
 
@@ -19,8 +21,17 @@ func init() {
 	log.SetLevel(log.InfoLevel)
 }
 
-
 func main() {
+	var s float64 = 0.0
+	for _, v := range config.Load {
+		s += v
+	}
+
+	log.WithFields(log.Fields{
+		"loads": config.Load,
+		"number of schedulers": *config.NumSchedulers,
+		"expected ratio": s / float64(*config.NumSchedulers),
+	}).Info("expected ratio")
 
 	ctl := controller.New()
 	ctl.AsServer()
