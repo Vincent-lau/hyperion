2b1804db2cff7b3a2d1a4f5518691ecbca8d246d
Shuntian Liu <shuntian.liu@outlook.com>
2022-07-29 18:12:17 +0100

    build: image pull if present

--- script/redeploy.sh
+++ script/redeploy.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
 kubectl delete -f deploy/my-controller-pod.yaml
-kubectl delete -f deploy/my-scheduler.yaml
+kubectl delete -f deploy/my-scheduler-deploy.yaml
 
 kubectl apply -f deploy/my-controller-pod.yaml
 
 sleep 5
 
-kubectl apply -f deploy/my-scheduler.yaml
+kubectl apply -f deploy/my-scheduler-deploy.yaml
--- script/run-sched.sh
+++ script/run-sched.sh
@@ -9,10 +9,8 @@ protoc --go_out=. --go_opt=paths=source_relative \
     message/message.proto
 
 
-go build -race -o ./bin/sched cmd/scheduler/main.go && \
+go build -o ./bin/sched cmd/scheduler/main.go && \
 docker build -t my-sched -f cmd/scheduler/Dockerfile . && \
 docker tag my-sched cuso4/my-sched && \
-docker push cuso4/my-sched && \
-kubectl delete -f deploy/my-scheduler.yaml && \
-kubectl apply -f deploy/my-scheduler.yaml
-
+kubectl delete -f deploy/my-scheduler-deploy.yaml && \
+kubectl apply -f deploy/my-scheduler-deploy.yaml
