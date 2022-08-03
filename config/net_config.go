d6359764ecfe2b66f3ae6ba519b6fbe07c18f939
Shuntian Liu <shuntian.liu@outlook.com>
2022-08-03 21:58:27 +0100

    style: namespace change

--- config/net_config.go
+++ config/net_config.go
@@ -7,8 +7,8 @@ import (
 var (
 	SchedPort = flag.String("port", "50051", "The server port")
 	CtlPort   = flag.String("ctlport", "50052", "The controller port")
-	SchedDNS  = flag.String("scheduler dns", "scheduler-headless.kube-system.svc.cluster.local", "DNS server")
-	CtlDNS    = flag.String("controller dns", "controller-svc.kube-system.svc.cluster.local", "controller DNS server")
+	SchedDNS  = flag.String("scheduler dns", "scheduler-headless.dist-sched.svc.cluster.local", "DNS server")
+	CtlDNS    = flag.String("controller dns", "controller-svc.dist-sched.svc.cluster.local", "controller DNS server")
 
 	StartPort = flag.String("startport", "50053", "The start port")
 
