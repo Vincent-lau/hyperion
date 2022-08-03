0f858118a48aeee2ea72d26a521281aef35ac050
Shuntian Liu <shuntian.liu@outlook.com>
2022-08-03 22:00:43 +0100

    chore: script formatting

--- script/run-mult.sh
+++ script/run-mult.sh
@@ -1,25 +1,31 @@
 #!/bin/zsh
 
 
-for run in {2..3}
+for run in 0
 do
-  echo "-------------------run $run-------------------"
-  for i in 50 100 150 200
+  gum style --foreground 212 --border-foreground 102 --border normal \
+            --align center --width 50 --margin "1 2" --padding "2 4" \
+            "run $run"
+  for i in 9
   do
-    echo "Running with $i pods"
+    gum style --foreground 103 --border-foreground 212 --border double \
+              --align center --width 50 --margin "1 2" --padding "2 4" \
+              "Running with $i pods"
 
     sed s/%REPLICAS%/$i/g deploy/my-scheduler-temp.yaml > deploy/my-scheduler-deploy.yaml 
     sed s/%REPLICAS%/$i/g deploy/my-controller-temp.yaml > deploy/my-controller-pod.yaml 
   
-    
     # make does not work, we need to wait for the controller a bit 
-    ./script/run.sh
+    CMD=$(gum choose "run.sh" "redeploy.sh" --limit=1 || echo "run.sh")
+    # CMD="run.sh"
+    ./script/$CMD
 
-    echo "waiting for algorithm to complete..."
-    sleep $i
+    # date
+    # gum spin -s line --title "waiting for algorithm to complete" sleep 200
+    # date
 
-    echo "writing results to data-${i}pods.json"
-    ./script/getmetrics.py > "measure/data/data-${i}pods-run${run}.json"
+    # gum style --foreground 212 "writing results to data-${i}pods-run${run}.json"
+    # ./script/getmetrics.py > "measure/data/data-${i}pods-run${run}.json"
 
   done
 done
