5aa22e6f1dbb82ba621f7a521cb9eae318e7faec
Shuntian Liu <shuntian.liu@outlook.com>
2022-08-09 19:21:15 +0100

    feat: gen new dir for each data

--- script/getprof.py
+++ script/getprof.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from kubernetes import client, config
-import os
+import os, datetime
 
 # Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
@@ -11,10 +11,13 @@ print("copying profiles from pods")
 ret = v1.list_pod_for_all_namespaces(watch=False)
 
 k = 0
+name = datetime.datetime.now().isoformat()[:-7].replace(':', '-')
+dir = f"measure/profiles/{name}/"
+os.mkdir(dir)
 for i in ret.items:
     if i.metadata.name.startswith("my-scheduler-"):
-      os.system(f'kubectl cp {i.metadata.name}:sched.prof measure/profiles/sched{k}.prof')
-      print(f'copied from {i.metadata.name} to sched{k}.prof')
+      os.system(f'kubectl cp {i.metadata.name}:sched.prof {dir}sched{k}.prof')
+      print(f'copied from {i.metadata.name} to {dir}sched{k}.prof')
       k += 1
     if k > 5:
       break
