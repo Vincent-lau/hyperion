b703b9a6651c07960decbc6595205632b2cbad3f
Shuntian Liu <shuntian.liu@outlook.com>
2022-08-15 16:55:59 +0100

    graph: plot placement stats

--- script/getcon.py
+++ script/getcon.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python3
 
-from kubernetes import client, config
-import json
 import datetime
-import os
-import sys
+import json
+import os, sys
+
+from kubernetes import client, config
 
 # Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
 
 
-stats_name = ['got jobs',
-              'initial w',
-              'final w',
-              "left elements",
-              "time taken"]
+stats_name = ['msg rcv total',
+              'msg sent total',
+              'comp time per iter',
+              'xchg time per iter',
+              'avg time per iter',
+              'total time',
+              'total iter',
+              'setup time']
 
 '''
 We analyse the following per pod:
+  msg rcv per iter
+  msg sent per iter
+  msg rcv total
+  msg sent total
+  total time per iter
+  compute time per iter
+  xchg time per iter
+  total time
+
 
 time unit is in microsecond 1e-6
 
@@ -35,10 +47,11 @@ for i in ret.items:
             name=i.metadata.name, namespace=i.metadata.namespace)
 
         sched_name = ""
-        if i.metadata.name.startswith('my-scheduler-') or i.metadata.name.startswith('my-controller'):
+        if i.metadata.name.startswith('my-scheduler-'):
             sched_name = i.metadata.name
         for line in lines.split('\n'):
-            if line.startswith('{') and line.find("placement") != -1 and line.find("trial") != -1:
+            if sched_name.startswith('my-sched') and line.startswith('{') and line.find("metrics") != -1 \
+                    and line.find("trial") != -1:
                 d = json.loads(line)
 
                 trial = int(d['trial']) - 1
@@ -49,8 +62,15 @@ for i in ret.items:
                     m[sched_name] = {}
 
                 for sn in stats_name:
+                    # if sn.endswith('per iter'):
+                    #     if not sn in m[sched_name]:
+                    #         m[sched_name][sn] = []
+                    #     if sn in d:
+                    #         m[sched_name][sn].append(d[sn])
+                    # else:
                     if sn in d:
                         m[sched_name][sn] = d[sn]
+
 
 
 name = datetime.datetime.now().isoformat()[:-7].replace(':', '-')
@@ -58,9 +78,5 @@ dir = f"measure/data/{name}"
 os.mkdir(dir)
 
 pods = int(sys.argv[1])
-fname = f"{dir}/placement-{pods}pods-60cap-2c.json"
-
-with open(fname, 'w') as f:
+with open(f"{dir}/metrics-{pods}pods.json", 'w') as f:
     json.dump(metrics, f)
-
-print(f"{fname} is created")
--- script/getmetrics.py
+++ script/getmetrics.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-import json, datetime, os
+import datetime
+import json
+import os
+import sys
 
 from kubernetes import client, config
 
@@ -8,14 +11,23 @@ from kubernetes import client, config
 config.load_kube_config()
 
 
-stats_name = ['msg rcv total',
-              'msg sent total',
-              'comp time per iter',
-              'xchg time per iter',
-              'avg time per iter',
-              'total time',
-              'total iter',
-              'setup time']
+consensus_stats_name = ['msg rcv total',
+                        'msg sent total',
+                        'comp time per iter',
+                        'xchg time per iter',
+                        'avg time per iter',
+                        'total time',
+                        'total iter',
+                        'setup time']
+
+placement_stats_name = [
+    "got jobs",
+    "initial w",
+    "final w",
+    "left elements",
+    "time taken",
+    "generated jobs size"
+]
 
 '''
 We analyse the following per pod:
@@ -37,43 +49,85 @@ metrics = {}
 
 
 v1 = client.CoreV1Api()
-ret = v1.list_namespaced_pod(namespace='dist-sched', watch=False)
-for i in ret.items:
-    if i.metadata.name.startswith("my-scheduler-") or i.metadata.name.startswith("my-controller"):
-        # print(f"{i.metadata.name} {i.status.pod_ip}")
-        lines = v1.read_namespaced_pod_log(
-            name=i.metadata.name, namespace=i.metadata.namespace)
-
-        sched_name = ""
-        if i.metadata.name.startswith('my-scheduler-'):
-            sched_name = i.metadata.name
-        for line in lines.split('\n'):
-            if sched_name.startswith('my-sched') and line.startswith('{') and line.find("metrics") != -1 \
-                    and line.find("trial") != -1:
-                d = json.loads(line)
-
-                trial = int(d['trial']) - 1
-                if len(metrics) < trial + 1:
-                    metrics[trial] = {}
-                m = metrics[trial]
-                if sched_name not in metrics[trial]:
-                    m[sched_name] = {}
-
-                for sn in stats_name:
-                    # if sn.endswith('per iter'):
-                    #     if not sn in m[sched_name]:
-                    #         m[sched_name][sn] = []
-                    #     if sn in d:
-                    #         m[sched_name][sn].append(d[sn])
-                    # else:
-                    if sn in d:
-                        m[sched_name][sn] = d[sn]
-
-
-
 name = datetime.datetime.now().isoformat()[:-7].replace(':', '-')
-dir = f"measure/data/{name}/"
+dir = f"measure/data/{name}"
 os.mkdir(dir)
 
-with open(f"{dir}/metrics.json", 'w') as f:
-    json.dump(metrics, f)
+
+def read_pl():
+    ret = v1.list_namespaced_pod(namespace='dist-sched', watch=False)
+    for i in ret.items:
+        if i.metadata.name.startswith("my-scheduler-") or i.metadata.name.startswith("my-controller"):
+            # print(f"{i.metadata.name} {i.status.pod_ip}")
+            lines = v1.read_namespaced_pod_log(
+                name=i.metadata.name, namespace=i.metadata.namespace)
+
+            sched_name = ""
+            if i.metadata.name.startswith('my-scheduler-') or i.metadata.name.startswith('my-controller'):
+                sched_name = i.metadata.name
+            for line in lines.split('\n'):
+                if line.startswith('{') and line.find("placement") != -1 and line.find("trial") != -1:
+                    d = json.loads(line)
+
+                    trial = int(d['trial']) - 1
+                    if len(metrics) < trial + 1:
+                        metrics[trial] = {}
+                    m = metrics[trial]
+                    if sched_name not in metrics[trial]:
+                        m[sched_name] = {}
+
+                    for sn in placement_stats_name:
+                        if sn in d:
+                            m[sched_name][sn] = d[sn]
+    pods = int(sys.argv[1])
+    fname = f"{dir}/placement-{pods}pods-60cap-2c.json"
+
+    with open(fname, 'w') as f:
+        json.dump(metrics, f)
+
+    print(f"{fname} is created for placement")
+
+
+def read_con():
+    ret = v1.list_namespaced_pod(namespace='dist-sched', watch=False)
+    for i in ret.items:
+        if i.metadata.name.startswith("my-scheduler-") or i.metadata.name.startswith("my-controller"):
+            # print(f"{i.metadata.name} {i.status.pod_ip}")
+            lines = v1.read_namespaced_pod_log(
+                name=i.metadata.name, namespace=i.metadata.namespace)
+
+            sched_name = ""
+            if i.metadata.name.startswith('my-scheduler-'):
+                sched_name = i.metadata.name
+            for line in lines.split('\n'):
+                if sched_name.startswith('my-sched') and line.startswith('{') and line.find("metrics") != -1 \
+                        and line.find("trial") != -1:
+                    d = json.loads(line)
+
+                    trial = int(d['trial']) - 1
+                    if len(metrics) < trial + 1:
+                        metrics[trial] = {}
+                    m = metrics[trial]
+                    if sched_name not in metrics[trial]:
+                        m[sched_name] = {}
+
+                    for sn in consensus_stats_name:
+                        # if sn.endswith('per iter'):
+                        #     if not sn in m[sched_name]:
+                        #         m[sched_name][sn] = []
+                        #     if sn in d:
+                        #         m[sched_name][sn].append(d[sn])
+                        # else:
+                        if sn in d:
+                            m[sched_name][sn] = d[sn]
+
+    pods = int(sys.argv[1])
+    fname = f"{dir}/consensus-{pods}pods.json"
+    with open(fname, 'w') as f:
+        json.dump(metrics, f)
+
+    print(f"{fname} is created for consensus")
+
+
+read_con()
+read_pl()
