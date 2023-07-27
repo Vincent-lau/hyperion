dbfd591d5687fdf5ee9fb43d7de437619a19f1d1
Shuntian Liu <shuntian.liu@outlook.com>
2023-07-27 15:33:40 +0100

    Allow parameterisation of whether we want to actually place a pod

--- internal/controller/k8s.go
+++ internal/controller/k8s.go
@@ -3,9 +3,11 @@ package controller
 import (
 	"context"
 	"fmt"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 
+	configs "github.com/Vincent-lau/hyperion/internal/configs"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -13,8 +15,6 @@ import (
 const (
 	schedulerName = "my-controller"
 )
-
-
 
 func (ctl *Controller) findNodes() {
 	// TODO add informer to get the list of nodes
@@ -85,64 +85,71 @@ func getJobDemand(pod *v1.Pod) float64 {
 	return float64(tot)
 }
 
+
+// Places pods/jobs to nodes, n is the name of the node and j is the index of the
+// job in the jobPod slice.
 func (ctl *Controller) placePodToNode(n string, j int) error {
-	// node := ctl.nodeMap[n]
-	// pod := ctl.jobPod[j]
+	var node *v1.Node
+	var pod *v1.Pod
+	if configs.K8sPlace {
+		node = ctl.nodeMap[n]
+		pod = ctl.jobPod[j]
 
-	// for {
-	// 	err := ctl.clientset.CoreV1().Pods(pod.Namespace).Bind(context.TODO(), &v1.Binding{
-	// 		ObjectMeta: metav1.ObjectMeta{
-	// 			Name:      pod.Name,
-	// 			Namespace: pod.Namespace,
-	// 		},
-	// 		Target: v1.ObjectReference{
-	// 			APIVersion: "v1",
-	// 			Kind:       "Node",
-	// 			Name:       node.Name,
-	// 		},
-	// 	}, metav1.CreateOptions{})
+		for {
+			err := ctl.clientset.CoreV1().Pods(pod.Namespace).Bind(context.TODO(), &v1.Binding{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      pod.Name,
+					Namespace: pod.Namespace,
+				},
+				Target: v1.ObjectReference{
+					APIVersion: "v1",
+					Kind:       "Node",
+					Name:       node.Name,
+				},
+			}, metav1.CreateOptions{})
 
-	// 	if err != nil {
-	// 		log.WithFields(log.Fields{
-	// 			"pod name":  pod.Name,
-	// 			"node name": node.Name,
-	// 			"error":     err,
-	// 		}).Warn("error binding pod to node")
-	// 	} else {
-	// 		log.WithFields(log.Fields{
-	// 			"pod name":  pod.Name,
-	// 			"node name": node.Name,
-	// 		}).Debug("successfully binding pod to node")
-	// 		break
-	// 	}
-	// }
+			if err != nil {
+				log.WithFields(log.Fields{
+					"pod name":  pod.Name,
+					"node name": node.Name,
+					"error":     err,
+				}).Warn("error binding pod to node")
+			} else {
+				log.WithFields(log.Fields{
+					"pod name":  pod.Name,
+					"node name": node.Name,
+				}).Debug("successfully binding pod to node")
+				break
+			}
+		}
+	}
 
 	ctl.placed <- j
 
-	// timestamp := time.Now().UTC()
-	// ctl.clientset.CoreV1().Events(pod.Namespace).Create(context.TODO(), &v1.Event{
-	// 	Count:          1,
-	// 	Message:        "binding pod to node",
-	// 	Reason:         "Scheduled",
-	// 	LastTimestamp:  metav1.NewTime(timestamp),
-	// 	FirstTimestamp: metav1.NewTime(timestamp),
-	// 	Type:           "Normal",
-	// 	Source: v1.EventSource{
-	// 		Component: schedulerName,
-	// 	},
-	// 	InvolvedObject: v1.ObjectReference{
-	// 		Kind:      "Pod",
-	// 		Name:      pod.Name,
-	// 		Namespace: pod.Namespace,
-	// 		UID:       pod.UID,
-	// 	},
-	// 	ObjectMeta: metav1.ObjectMeta{
-	// 		GenerateName: pod.Name + "-",
-	// 	},
-	// }, metav1.CreateOptions{})
+	if configs.K8sPlace {
+		timestamp := time.Now().UTC()
+		ctl.clientset.CoreV1().Events(pod.Namespace).Create(context.TODO(), &v1.Event{
+			Count:          1,
+			Message:        "binding pod to node",
+			Reason:         "Scheduled",
+			LastTimestamp:  metav1.NewTime(timestamp),
+			FirstTimestamp: metav1.NewTime(timestamp),
+			Type:           "Normal",
+			Source: v1.EventSource{
+				Component: schedulerName,
+			},
+			InvolvedObject: v1.ObjectReference{
+				Kind:      "Pod",
+				Name:      pod.Name,
+				Namespace: pod.Namespace,
+				UID:       pod.UID,
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: pod.Name + "-",
+			},
+		}, metav1.CreateOptions{})
+	}
 
 	return nil
 
 }
-
-
