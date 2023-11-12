63dc906352809536b5e0fd7ab37ed32f36afb73b
Shuntian Liu <shuntian.liu@outlook.com>
2023-11-12 22:24:33 +0000

    Add readme and more explanation in script

--- README.md
+++ README.md
@@ -1,22 +1,47 @@
 # Distributed Scheduler for Kubernetes
 
 
+
 ## Directory structre
 
 ```
 .
 ├── bin # binary for scheudler and controller
+├── build
+│   └── package # Dockerfiles
 ├── cmd # main files for scheduler and controller
 │   ├── controller
+│   ├── rand_sched
 │   └── scheduler
-├── config  # some network configuration
-│   └── data # network topology
-├── controller # controller for setting up the graph
 ├── deploy # k8s deployment files
-├── message # protobuf
-├── scheduler # main scheduler source code
-├── script
-└── server
+│   ├── bbox
+│   │   └── templates
+│   ├── elasticsearch
+│   ├── fluentd
+│   ├── kibana
+│   ├── templates
+│   └── workload
+│       ├── fft
+│       └── fibonacci
+├── internal # main hyperion implementation
+│   ├── configs # scheduler configs
+│   │   └── data
+│   ├── controller
+│   ├── message # Protobuf
+│   ├── metrics # prometheus
+│   ├── rand_sched
+│   ├── scheduler
+│   ├── server
+│   └── util
+├── log
+├── measure
+│   ├── data
+│   ├── graphs
+│   ├── old_data
+│   ├── profiles
+│   └── trace
+│       └── 2022-08-23T17-11-56
+│           └── trace
+└── scripts # scripts to invoke and deploy schedulers and jobs
+    └── __pycache__
 ```
-
-
--- scripts/run-sched.py
+++ scripts/run-sched.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Purpose: Run the scheduler with a given number of pods and jobs
+# Purpose: Run the scheduler with a given number of schedulers and jobs
 
 from jinja2 import Environment, FileSystemLoader
 import pytermgui as ptg
@@ -49,25 +49,25 @@ def run_sched():
     ''')
 
 
-def render_sched(pods: int, mode: Mode, top: int):
+def render_sched(schedulers: int, mode: Mode, top: int):
     environment = Environment(loader=FileSystemLoader("deploy/templates"))
     template = environment.get_template("my-scheduler.yaml.jinja2")
     with open('deploy/my-scheduler.yaml', 'w') as out_file:
         content = template.render(
-            replicas=pods,
+            replicas=schedulers,
             mode=mode.name,
             topid=top
         )
         out_file.write(content)
 
 
-def render_ctrler(pods: int, mode: Mode, top: int, job_factor: int, trials: int):
+def render_ctrler(schedulers: int, mode: Mode, top: int, job_factor: int, trials: int):
     environment = Environment(loader=FileSystemLoader("deploy/templates"))
     template = environment.get_template("my-controller.yaml.jinja2")
     trials = 1
     with open('deploy/my-controller.yaml', 'w') as out_file:
         content = template.render(
-            replicas=pods,
+            replicas=schedulers,
             mode=mode.name,
             topid=top,
             trials=trials,
@@ -95,15 +95,15 @@ def main():
 
     parser.add_argument('-m', '--make', action='store_true', help='run make')
     parser.add_argument('-d', '--dev', action='store_true',
-                        help='run in production mode, default is prod')
+                        help='run in development mode, default is prod')
     parser.add_argument('-r', '--remove', action='store_true',
-                        help='remove previous the controller and scheduler')
-    parser.add_argument('-p', '--pods', type=int, nargs='+',
-                        help='number of pods to run', required=True)
+                        help='remove/undeploy previous controller and scheduler')
+    parser.add_argument('-s', '--schedulers', type=int, nargs='+',
+                        help='number of schedulers to run')
     parser.add_argument('-t', '--topology', type=int,
-                        nargs='+', help='topology id')
+                        nargs='+', help='topology id, see internal/configs/data')
     parser.add_argument('-j', '--jobs', type=int, nargs='+',
-                        help='job factor, the job/scheduler ratio, default to 1')
+                        help='job factor, the job/scheduler ratio, i.e., how many jobs we want per scheduler, default to 1')
     parser.add_argument('-w', '--wait', type=int,
                         help='wait time for job completion in seconds')
     parser.add_argument('-l', '--logs', action='store_true', default=False,
@@ -130,31 +130,31 @@ def main():
 
     os.system('kubectl config set-context --current --namespace=dist-sched')
 
-    start_pods = 500
-    end_pods = 1000
-    if len(args.pods) > 1:
-        start_pods = args.pods[0]
-        end_pods = args.pods[1]
-    elif len(args.pods) == 1:
-        start_pods = args.pods[0]
-        end_pods = start_pods + 100
+    start_sched = 500
+    end_sched = 1000
+    if len(args.schedulers) > 1:
+        start_sched = args.schedulers[0]
+        end_sched = args.schedulers[1]
+    elif len(args.schedulers) == 1:
+        start_sched = args.schedulers[0]
+        end_sched = start_sched + 100
 
     for _ in range(args.run):
         metrics_getter = None
-        for pods in args.pods:
+        for sched in args.schedulers:
             for jobs in args.jobs if args.jobs else [1]:
-                for top in args.topology if args.topology else [2]:
+                for top in args.topology if args.topology else [1]:
                     print(
-                        f"Running with {pods} pods and {jobs * pods} jobs {top} topology")
+                        f"Running with {sched} schedulers and {jobs * sched} jobs {top} topology")
                     remove()
-                    render_ctrler(pods, mode, top, jobs, 1)
-                    render_sched(pods, mode, top)
+                    render_ctrler(sched, mode, top, jobs, 1)
+                    render_sched(sched, mode, top)
                     run_ctrler()
                     print("waiting for controller to start up")
                     time.sleep(10)
                     run_sched()
 
-                    sleep_time = args.wait if args.wait else max(200, pods)
+                    sleep_time = args.wait if args.wait else max(200, sched)
                     try:
                         for i in range(sleep_time):
                             print(
@@ -165,7 +165,7 @@ def main():
                     if mode == Mode.PROD or args.logs:
                         metrics_getter = getmetrics.MetricsGetter(
                         ) if not metrics_getter else metrics_getter
-                        metrics_getter.getmetrics(pods, jobs, top, args.logs)
+                        metrics_getter.getmetrics(sched, jobs, top, args.logs)
 
 
 if __name__ == "__main__":
